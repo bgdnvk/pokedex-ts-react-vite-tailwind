e5156957044ce04d334b6fee1371c07b24f34ccf
bgdnvk <bnovykov@gmail.com>
2022-08-21 20:36:52 +0200

    CSS is pain: PokeList n Card mod

--- src/components/poke-list/PokeList.tsx
+++ src/components/poke-list/PokeList.tsx
@@ -4,12 +4,13 @@ import PokeItem from "./PokeItem"
 const PokeList = ({allPokemon, formData}:{allPokemon: any, formData: any}) => {
 
     return(
-        <div className="flex justify-center">
-          <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
+        <div className="flex flex-wrap border-2 border-red-500">
+          {/* <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm"> */}
+          <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm border-2 border-green-900">
             <h1 className="text-gray-900 text-xl leading-tight font-medium mb-2">
               filter pokemon
             </h1>
-            <ol className="p-6 divide-y divide-slate-200">
+            <ul className="p-6 divide-y divide-slate-200 border-2 border-red-500">
               {allPokemon?.results
                 // ?.slice(0-999)
                 ?.filter((e: { name: string }) =>
@@ -20,7 +21,7 @@ const PokeList = ({allPokemon, formData}:{allPokemon: any, formData: any}) => {
                     <PokeItem name={e.name} url={e.url}></PokeItem>
                   </li>
                 ))}
-            </ol>
+            </ul>
           </div>
         </div>
     )
