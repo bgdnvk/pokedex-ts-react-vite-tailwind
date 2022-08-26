64ad5f869fdb1644d4e4a48152bb45cfea7bd742
bgdnvk <bnovykov@gmail.com>
2022-08-26 22:10:38 +0200

    about page

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -2,6 +2,9 @@ const About = () => {
     return(
         <main>
             <h1>About page</h1>
+            <p>this page is using the v2 API from <a href="https://pokeapi.co">PokeApi</a></p>
+            <p>author: <a href="https://github.com/bgdnvk">@bgdnvk on Github</a> or
+            <a href="https://twitter.com/tekbog">@tekbog on Twitter</a>  </p>
         </main>
     )
 }
